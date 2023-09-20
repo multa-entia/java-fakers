2861c42ee8d17c57fe6fa01b1616e593d6703179
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-09-20 23:05:03 +0300

    DefaultStringFakerTemplateParser is done

--- src/main/java/ru/multa/entia/fakers/impl/strf/DefaultStringFakerTemplateParser.java
+++ src/main/java/ru/multa/entia/fakers/impl/strf/DefaultStringFakerTemplateParser.java
@@ -1,10 +1,16 @@
 package ru.multa.entia.fakers.impl.strf;
 
+import ru.multa.entia.fakers.api.strf.StringFakerSetting;
 import ru.multa.entia.fakers.api.strf.StringFakerSettingParser;
 import ru.multa.entia.fakers.api.strf.StringFakerTemplate;
 import ru.multa.entia.fakers.api.strf.StringFakerTemplateParser;
 
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DefaultStringFakerTemplateParser implements StringFakerTemplateParser {
+    private static final String REGEX = "\\[[^{}]+\\]\\{[0-9]+:[0-9]+\\}";
     private static final String LABEL_TEMPLATE = "<@label_%s>";
 
     private final StringFakerSettingParser settingParser;
@@ -13,15 +19,32 @@ public class DefaultStringFakerTemplateParser implements StringFakerTemplatePars
         this(new DefaultStringFakerSettingParser());
     }
 
-    public DefaultStringFakerTemplateParser(StringFakerSettingParser settingParser) {
+    public DefaultStringFakerTemplateParser(final StringFakerSettingParser settingParser) {
         this.settingParser = settingParser;
     }
 
     @Override
-    public StringFakerTemplate parse(String template) {
-        // TODO: 19.09.2023 !!!
-//        text [wwf]{wefwef} qenkjefb []{} adqdqwd
-//        String template = "\\[.+\\]\\{[0-9]+:[0-9]+\\}";
-        return null;
+    public StringFakerTemplate parse(final String template) {
+        String line = template;
+
+        HashMap<String, StringFakerSetting> settings = new HashMap<>();
+
+        int counter = 0;
+        final Pattern pattern = Pattern.compile(REGEX);
+        while (true) {
+            final Matcher matcher = pattern.matcher(line);
+            if (matcher.find()){
+                String substring = line.substring(matcher.start(), matcher.end());
+
+                String label = String.format(LABEL_TEMPLATE, counter++);
+                line = line.replaceFirst(REGEX, label);
+
+                settings.put(label, settingParser.parse(substring));
+            } else {
+                break;
+            }
+        }
+
+        return new DefaultStringFakerTemplate(line, settings);
     }
 }
--- src/test/java/ru/multa/entia/fakers/impl/strf/DefaultStringFakerTemplateParserTest.java
+++ src/test/java/ru/multa/entia/fakers/impl/strf/DefaultStringFakerTemplateParserTest.java
@@ -1,20 +1,35 @@
 package ru.multa.entia.fakers.impl.strf;
 
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import ru.multa.entia.fakers.api.strf.StringFakerSetting;
 import ru.multa.entia.fakers.api.strf.StringFakerSettingParser;
+import ru.multa.entia.fakers.api.strf.StringFakerTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DefaultStringFakerTemplateParserTest {
 
     @Test
     void shouldCheckParsing() {
-        DefaultStringFakerTemplateParser parser = new DefaultStringFakerTemplateParser(
-                new TestStringFakerSettingParser()
-        );
+        String template = "prefix [012]{1:3} middle [a-z]{3:7} suffix";
+        String expectedTemplate = "prefix <@label_0> middle <@label_1> suffix";
 
+        TestStringFakerSettingParser settingParser = new TestStringFakerSettingParser();
+        Map<String, StringFakerSetting> expectedSettings = new HashMap<>(){{
+            put("<@label_0>", settingParser.parse("[012]{1:3}"));
+            put("<@label_1>", settingParser.parse("[a-z]{3:7}"));
+        }};
 
+        DefaultStringFakerTemplateParser parser = new DefaultStringFakerTemplateParser(settingParser);
+        StringFakerTemplate result = parser.parse(template);
+
+        assertThat(result.template()).isEqualTo(expectedTemplate);
+        assertThat(result.settings()).isEqualTo(expectedSettings);
     }
 
     private static class TestStringFakerSettingParser implements StringFakerSettingParser {
@@ -25,7 +40,24 @@ class DefaultStringFakerTemplateParserTest {
             for (int i = 0; i < length; i++) {
                 charCodes[i] = line.charAt(i);
             }
-            return new DefaultStringFakerSetting(0, 0, charCodes);
+            return new TestStringFakerSetting(0, 0, charCodes);
         }
+    }
+
+    @RequiredArgsConstructor
+    @EqualsAndHashCode
+    private static class TestStringFakerSetting implements StringFakerSetting {
+        private final int minLen ;
+        private final int maxLen;
+        private final int[] charCodes;
+
+        @Override
+        public int minLen() { return minLen; }
+
+        @Override
+        public int maxLen() { return maxLen; }
+
+        @Override
+        public int[] charCodes() { return charCodes; }
     }
 }
